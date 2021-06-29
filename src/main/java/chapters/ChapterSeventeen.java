d9aaeb5b146ac86bef8329fafebdeabdc87e7f0c
Nick Kang <cleverdevk@gmail.com>
2021-06-29 20:00:24 +0900

    Done - Chapter Seventeen

--- src/main/java/chapters/ChapterSeventeen.java
+++ src/main/java/chapters/ChapterSeventeen.java
@@ -6,8 +6,10 @@ import io.reactivex.Observer;
 import lombok.extern.slf4j.Slf4j;
 import objects.temperature.*;
 
+import java.util.Arrays;
 import java.util.concurrent.Flow;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class ChapterSeventeen {
@@ -23,10 +25,13 @@ public class ChapterSeventeen {
         Observable<Long> onePerSec = Observable.interval(1, TimeUnit.SECONDS);
 
         onePerSec.blockingSubscribe(i -> System.out.println(TempInfo.fetch("New York")));
-        Observable<TempInfo> tempObservable =  getTemperatureWithRxJava("new york", 10);
+        Observable<TempInfo> tempObservable =  getTemperatureWithRxJava("new york", 5);
         tempObservable.blockingSubscribe(new TempObserver());
+        Observable<TempInfo> celsiusTempObservable = getCelsiusTemperatureWithRxJava("new york", 5);
+        celsiusTempObservable.blockingSubscribe(new TempObserver());
 
-
+        Observable<TempInfo> observable = getCelsiusTemperatures(5, "NEWYORK", "SEOUL");
+        observable.blockingSubscribe(new TempObserver());
     }
 
     private static Flow.Publisher<TempInfo> getTemperature(String town) {
@@ -54,5 +59,20 @@ public class ChapterSeventeen {
                         }
                     });
         });
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperatureWithRxJava(String town, int n) {
+        return getTemperatureWithRxJava(town, n)
+                .map(temp -> new TempInfo(temp.getTown(), (temp.getTemp() - 32) * 5 /9));
+    }
+    public static Observable<TempInfo> getCelsiusTemperaturePositiveWithRxJava(String town, int n) {
+        return getTemperatureWithRxJava(town, n)
+                .map(temp -> new TempInfo(temp.getTown(), (temp.getTemp() - 32) * 5 /9))
+                .filter(temp -> temp.getTemp() > 0);
+    }
+    public static Observable<TempInfo> getCelsiusTemperatures(int n, String... towns) {
+        return Observable.merge(Arrays.stream(towns)
+                .map(town -> getCelsiusTemperatureWithRxJava(town, n))
+                .collect(Collectors.toList()));
     }
 }
